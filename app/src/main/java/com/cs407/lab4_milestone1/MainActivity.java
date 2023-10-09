01e65dd55ee88fa96440077a01f506ce327829b6
ShruthiPrabaharasundar <prabaharasun@wisc.edu>
2023-10-09 01:31:51 -0500

    2nd update

--- app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
@@ -3,12 +3,30 @@ package com.cs407.lab4_milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void mockFileDownloader(){
+        for(int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void startDownload(View view){
+        mockFileDownloader();
+    }
+
 }
+

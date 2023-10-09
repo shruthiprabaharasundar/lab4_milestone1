058e12e02f2260eb1d7933b68b9d0de9dca3323c
ShruthiPrabaharasundar <prabaharasun@wisc.edu>
2023-10-09 01:13:20 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lab4_milestone1"
+include(":app")

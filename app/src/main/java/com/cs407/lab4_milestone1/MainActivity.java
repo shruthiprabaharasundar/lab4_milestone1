76506623efa4b7a2041eaf5a3126a382a2e0eaa8
shruth <prabaharasun@wisc.edu>
2023-10-11 18:08:29 -0500

    4th commit

--- app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
+++ app/src/main/java/com/cs407/lab4_milestone1/MainActivity.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.button1);
     }
 
     public void mockFileDownloader(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading..");
+            }
+        });
+
         for(int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
             Log.d(TAG, "Download Progress: " + downloadProgress + "%");
             try {
@@ -23,10 +35,26 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                startButton.setText("Start");
+            }
+        });
     }
     public void startDownload(View view){
-        mockFileDownloader();
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
     }
-
+    public void stopDownload(View view){
+        stopThread = true;
+    }
+    class ExampleRunnable implements Runnable{
+        @Override
+        public void run(){
+            mockFileDownloader();
+        }
+    }
 }
 
